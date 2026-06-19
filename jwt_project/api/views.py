1cbe191004a5449ee0c654a6a05701de04c5004d
oguevara-code <oguevara@unsa.edu.pe>
2026-06-18 21:10:37 -0500

    Implementando autenticacion JWT y endpoint protegido

--- jwt_project/api/views.py
+++ jwt_project/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def saludo(request):
+
+    return Response({
+        "mensaje": "Autenticacion JWT correcta"
+    })
